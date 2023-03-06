cc27052784c0162e0258c6f4a0abac4bfb8fee95
Firdhaus Dwi Sukma <firdausdwisukma@gmail.com>
2023-03-06 13:26:31 +0700

    initial commit

---
+++ src/Inventory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class Inventory{
+
+    private array $products;
+
+    // public function getProducts(){
+    //     $productsRepo = new ProductRepository()
+    // }
+}
