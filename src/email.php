2c3f86c037be481d6bb53ae2c34a08329d198243
FirdhausDwiSukma <firdausdwisukma@gmail.com>
2023-03-06 15:54:37 +0700

    emailtesting

--- src/email.php
+++ src/email.php
@@ -1,21 +1,26 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App;
 
-final class email{
+final class Email
+{
     private string $email;
-    
-    private function __construct(string $email){
-        $this -> ensureIsValidEmail($email);
-        $this -> email = $email;
+
+    private function __construct(string $email)
+    {
+        $this->ensureIsValidEmail($email);
+
+        $this->email = $email;
     }
 
-    public static function fromString(string $email): self {
+    public static function fromString(string $email): self
+    {
         return new self($email);
     }
 
-    public function asString(): string {
-        return $this-> email;
+    public function asString(): string
+    {
+        return $this->email;
     }
 
     private function ensureIsValidEmail(string $email): void
